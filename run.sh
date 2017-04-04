ef2f75874fe35ae0ca5afbcf338f84ea7eb82cf1
orangekmt <kmt.team@orange.com>
2017-04-04 18:21:13 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 set -e
 
+echo "" >> /.s3cfg
+echo "access_key = ${aws_key}" >> /opt/.s3cfg
+echo "secret_key = ${aws_secret}" >> /opt/.s3cfg
+echo "host_base = ${s3_host_base}" >> /opt/.s3cfg
+echo "host_bucket = ${s3_host_bucket}" >> /opt/.s3cfg
+
 exec tail -f /dev/null
