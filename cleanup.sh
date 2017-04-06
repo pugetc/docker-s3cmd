916a1333a2c1c117227c79faac29210d155923de
orangekmt <kmt.team@orange.com>
2017-04-06 18:59:48 +0300

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -8,7 +8,7 @@ s3cmd ls $1 | grep " DIR " -v | while read -r line;
   do
     createDate=`echo $line|awk {'print $1" "$2'}`
     createDate=`date -d "$createDate" +%s`
-    olderThan=`date -d "-${CONFIG_rotation_remote} days" +%s`
+    olderThan=`date -d "-$2 days" +%s`
     if [[ $createDate -lt $olderThan ]]
       then
         fileName=`echo $line|awk {'print $4'}`
@@ -17,7 +17,7 @@ s3cmd ls $1 | grep " DIR " -v | while read -r line;
             printf 'Deleting "%s"\n' $fileName
             s3cmd del "$fileName"
           else
-            printf "\n No files to delete. Bucket ${CONFIG_backup_dir_remote} directory is empty."
+            printf "\n No files to delete. Bucket's $1 directory is empty."
         fi
       else
            fileName=`echo $line|awk {'print $4'}`
